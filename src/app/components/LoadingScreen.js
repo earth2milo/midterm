fba6b30a49d4cb86da252f092f2bf1abfec27d1f
earth2milo <90530636+earth2milo@users.noreply.github.com>
2023-10-30 20:53:05 -0400

    final edits

--- src/app/components/LoadingScreen.js
+++ src/app/components/LoadingScreen.js
@@ -4,6 +4,9 @@ import "./LoadingScreen.css";
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
+      <div className="logoWrap">
+        < img className="logo" src="/Milo_Logo.png" alt="Milo Soriano Portfolio Logo" />
+      </div>
       <div className="loading-bar">
         <div className="loading-text">The Wiki & Met API is Generating your data</div>
         <div className="progress-bar"></div>
--- src/app/components/api.js
+++ src/app/components/api.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "axios"; 
+
+// this is the http client library that i found to bypass CORS and make requests to my two apis
+// + to install axios i did npm install axios on my command line for the project.
+
+
 import "../../Metmuseum.Module.css";
 import LoadingScreen from "./LoadingScreen";
 
-const API_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1";
+const API_BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1"; // i'm setting the base URL and startpoint for the met api
 
-const MetMuseumComponent = () => {
+
+
+// for this component, i need to set a random artwork
+// a way to define the random artwork that i will get from my math.floor statement
+// and then I need to define my wikipedia article, and the chosen wikipedia article based on the randomArtwork chosen
+// and finally, a loading statement, because after many tries, I decided to add a loading screen for these two apis, because
+// i kept getting errors from making too many requests, or the wikipedia article would sometimes fully take 20+ seconds to find an article.
+// the loading and SetLoading helped debug a lot.
+
+
+const MetMuseumComponent = () => { 
   const [randomArtwork, setRandomArtwork] = useState(null);
   const [wikipediaArticle, setWikipediaArticle] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -13,18 +28,30 @@ const MetMuseumComponent = () => {
   useEffect(() => {
     const fetchRandomArtwork = async () => {
       try {
+
+
         // Fetch a list of all object IDs from the Met Museum API
-        const response = await axios.get(`${API_BASE_URL}/objects`);
-        const allObjectIDs = response.data.objectIDs;
+        // so i feel like this is where axios really shines when I define my base url and i'm able to create two new 
+        // constants to setup my math.floor statement
+
+
+        const response = await axios.get(`${API_BASE_URL}/objects`); 
+        const allObjectIDs = response.data.objectIDs; 
 
         // Select a random object ID from the list
-        const randomObjectID = allObjectIDs[Math.floor(Math.random() * allObjectIDs.length)];
+        
+        const randomObjectID = allObjectIDs[Math.floor(Math.random() * allObjectIDs.length)]; 
 
         // Fetch the details of the randomly selected artwork
+        
         const artworkResponse = await axios.get(`${API_BASE_URL}/objects/${randomObjectID}`);
         const fetchedArtwork = artworkResponse.data;
 
+
         // Extract additional information from the artwork response
+        // ^ so these are all the additional info i am pulling from the met api that i grabbed from their documentation.
+
+      
         const additionalInfo = {
           department: fetchedArtwork.department,
           accessionYear: fetchedArtwork.accessionYear,
@@ -33,7 +60,17 @@ const MetMuseumComponent = () => {
           artistDisplayName: fetchedArtwork.artistDisplayName,
         };
 
+
+// so while coding this project, 90% of it was just spent debugging api responses, and finding that a lot of the randomly
+// generated artwork had inconsistent amounts of info filled out for the item
+// like a vase made in 500 BC would be missing an artistDisplayName
+// So I would try to filter by culture and period display info
+//  // // tldr: I made a set of if statements for my selected random artwork to filter through options of info that 
+// the artwork would have. 
         // Attempt to fetch Wikipedia data based on culture first
+
+
+  
         let wikiResponse = await searchWikipedia(fetchedArtwork.culture);
       
         // If no result for culture, try fetching based on period
@@ -45,6 +82,11 @@ const MetMuseumComponent = () => {
         if (!wikiResponse) {
           wikiResponse = await searchWikipedia(fetchedArtwork.artistDisplayName);
         }
+
+
+// documentation for catching errors, and overall just making sure i get really clear error statements 
+// for myself while coding.
+
 
         if (wikiResponse) {
           setWikipediaArticle(wikiResponse);
@@ -61,20 +103,42 @@ const MetMuseumComponent = () => {
       }
     };
 
+
+// basically once the metapi is fully ready to go and has found it's info, now the real work with making it dynamic begins.
+// i have to define a search term variable and filter by possible info categories from the random artwork to find a page to 
+// display below the artwork. 
+// 
+// +++ if I had to make improvements to this, I would definitely want to show the user what the wikipedia page api searched for
+// so that the user can tell if the information is relevant or not.
+// because I found that when the code defaults to searching for a culture, it just says generic info about a country that is 
+// not super detailed.
+
+
     const searchWikipedia = async (searchTerm) => {
       try {
         const response = await axios.get(`https://en.wikipedia.org/api/rest_v1/page/summary/${searchTerm}`);
         
         // Check if the response indicates a disambiguation page
+        // so for wikipedia, a disambiguation page are hatnotes or summaries of search results.
+        // this if statement is basically checking if the responses from the summaries are pulling up a page or not.
+        // this is why I need a loading page :///
+
+
         if (response.data.type === "https://mediawiki.org/wiki/HyperSwitch/errors/not_found" ||
             response.data.type === "https://en.wikipedia.org/api/rest_v1/page/other" ||
             response.data.type === "disambiguation") {
-          // Handle disambiguation page, or set null if you want to skip it
+  
           console.log("Disambiguation page found for:", searchTerm);
           return null;
         }
         
         // Check if the response contains relevant information
+        // if we pass the disambiguation page stage
+        // we move onto pulling relevant data for our artwork
+        // if not, we move to getting accurate and correct console.logs for us to debug, with it either being an empty response
+        // or an unknown error
+
+
         if (response.data.title && response.data.extract) {
           return response.data;
         } else {
@@ -98,32 +162,49 @@ const MetMuseumComponent = () => {
     <div className="artwork-container">
       {randomArtwork && wikipediaArticle ? (
         <div>
-          <h1>Artwork of the Day</h1>
-          <div className="img-wrapper">
-            <h2 className="artwork-title">{randomArtwork.title}</h2>
-            <img className="artwork-image" src={randomArtwork.primaryImage} alt={randomArtwork.title} />
+          <div className="titleWrap">
+          <h1 className="header">Artwork of the Day</h1>
           </div>
+          <div className="img-wrapper">
+
+            <img className="artwork-image" src={randomArtwork.primaryImage} alt={randomArtwork.title} />
+            <h2 className="artwork-title">{randomArtwork.title}</h2>
+
+          </div>
+
           <div className="additional-info">
+
             <h2>Additional Information</h2>
+
             {randomArtwork.department && <p><strong>Department:</strong> {randomArtwork.department}</p>}
             {randomArtwork.accessionYear && <p><strong>Accession Year:</strong> {randomArtwork.accessionYear}</p>}
             {randomArtwork.culture && <p><strong>Culture:</strong> {randomArtwork.culture}</p>}
             {randomArtwork.period && <p><strong>Period:</strong> {randomArtwork.period}</p>}
-            {randomArtwork.artistDisplayName && <p><strong>Artist Display Name:</strong> {randomArtwork.artistDisplayName}</p>}
+            {randomArtwork.artistDisplayName && <p><strong>Artist Name:</strong> {randomArtwork.artistDisplayName}</p>}
+
           </div>
-          <h1>What does Wikipedia have to say about this work?</h1>
+
+          <h1 className="header">What does Wikipedia have to say about this work?</h1>
+
           <div className="wikipedia-article">
+
             <h2>{wikipediaArticle.title}</h2>
             <p>{wikipediaArticle.extract}</p>
+
           </div>
+
         </div>
+
       ) : (
         <div>
+
           <LoadingScreen/>
           <p>Oh no! The Met API tripped and fell on the way to work! and there is no information available for the selected artwork. Please wait for it to load!</p>
           <p>If it takes too long just refresh!</p>
+
         </div>
       )}
+
     </div>
   );
 };
